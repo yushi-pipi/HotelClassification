09c2ae4726bc557201bfc6d9e2f6d2dbf6a2ebdd
ikeda <acro.ikeda@gmail.com>
2020-12-06 22:25:57 +0900

    linbotfirst2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,abort
 from linebot import(LineBotApi,WebhookHandler)
 from linebot.exceptions import(InvalidSignatureError)
-from linebot.modelsimport(MessageEvent,TextMessage,TextSendMessage)
+from linebot.models import(MessageEvent,TextMessage,TextSendMessage)
 
 
 app=Flask(__name__)
