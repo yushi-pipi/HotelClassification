f864078caefeae511acdb284d3ab0e22132bc862
ikeda <acro.ikeda@gmail.com>
2020-12-06 22:21:02 +0900

    linbotfirst

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 from flask import Flask,request,abort
 from linebot import(LineBotApi,WebhookHandler)
 from linebot.exceptions import(InvalidSignatureError)
-from linebot.models import(MessageEvent,TextMessage,TextSendMessage)
+from linebot.modelsimport(MessageEvent,TextMessage,TextSendMessage)
 
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 ACCESS_TOKEN = "/qpqZBhWUEmB1pyOT9pvqMsNaJnTzDc2RF9SXzl2PckRRONWt/PViT9RC8+mVQlYcTCd33zjcKv7QCGD4LCOH64p0QQ7FhAQXMgHKB1O/TaxzMPeiY0h4NeRdhEPU0anNjfHs1pteYicz7m/1wSwqAdB04t89/1O/w1cDnyilFU="
 SECRET = "5668269638be4261988bd1092008c514"
 
-line_bot_api = LineBotApi(ACCESS_TOKEN)
-handler = WebhookHandler(SECRET)
 
-@app.route('/collback',methods=['POST'])
-def collback():
-    signature = request.handlers['X-Line-Signature']
+line_bot_api=LineBotApi(ACCESS_TOKEN)
+handler=WebhookHandler(SECRET)
 
-    body = request.get_data(as_text=True)
-    app.logger.info('Request body: ' + body)
-
+@app.route("/callback",methods=['POST'])
+def callback():
+    signature=request.headers['XLineSignature']
+    body=request.get_data(as_text=True)
+    app.logger.info("Requestbody:"+body)
     try:
         handler.handle(body,signature)
-    except :
+    except InvalidSignatureError:
         abort(400)
-
-    return 'OK'
-
+        
+    return'OK'
+    
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
